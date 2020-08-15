628a88a091903392eb9dd3943e31674f9f1e482c
Matheus Carneiro <matheus10carneiro@gmail.com>
2020-08-15 12:10:17 -0300

    ALTERACOES LAYOUT/MELHORAMENTOS

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,15 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-const request = "https://api.hgbrasil.com/weather?key=4c4dc19b&user_ip=remote";
 
 void main() async {
   runApp(MaterialApp(
+    title: "Weater App",
     home: Home(),
-    theme: ThemeData(hintColor: Colors.blue, primaryColor: Colors.white),
   ));
-}
-
-Future<Map> getData() async {
-  http.Response response = await http.get(request);
-  return json.decode(response.body);
 }
 
 class Home extends StatefulWidget {
@@ -22,9 +18,119 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  Future<Map> getWeather() async {
+    http.Response response = await http.get("https://api.hgbrasil.com/weather?key=4c4dc19b&user_ip=remote");
+    return json.decode(response.body);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: FutureBuilder<Map> (
+        future: getWeather(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return Center(
+                child: Text(
+                  "Loading...",
+                  style: TextStyle(color: Colors.white, fontSize: 25.0),
+                  textAlign: TextAlign.center,
+                ),
+              );
+            default:
+              if(snapshot.hasError) {
+                return Center(
+                  child: Text(
+                    "Error is loading data...",
+                    style: TextStyle(color: Colors.white, fontSize: 25.0),
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              } else {
+                return Column(
+                  children: [
+                    Container(
+                      height: MediaQuery.of(context).size.height / 4,
+                      width: MediaQuery.of(context).size.width,
+                      color: Colors.red,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(bottom: 10.0),
+                            child: Text(
+                              snapshot.data["results"]["city_name"],
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 14.0,
+                                  fontWeight: FontWeight.w600
+                              ),
+                            ),
+                          ),
+                          Text(
+                            snapshot.data["results"]["temp"].toString() + "°C",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 40.0,
+                                fontWeight: FontWeight.w600
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(top: 10.0),
+                            child: Text(
+                              snapshot.data["results"]["description"],
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 14.0,
+                                  fontWeight: FontWeight.w600
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.all(20.0),
+                        child: ListView(
+                          children: [
+                            ListTile(
+                              leading: FaIcon(FontAwesomeIcons.thermometerHalf),
+                              title: Text("Temperature"),
+                              trailing: Text(snapshot.data["results"]["temp"].toString() + "°C"),
+                            ),
+                            ListTile(
+                              leading: FaIcon(FontAwesomeIcons.cloud),
+                              title: Text("Weather"),
+                              trailing: Text(snapshot.data["results"]["description"]),
+                            ),
+                            ListTile(
+                              leading: FaIcon(FontAwesomeIcons.sun),
+                              title: Text("Humidity"),
+                              trailing: Text(snapshot.data["results"]["humidity"].toString()),
+                            ),
+                            ListTile(
+                              leading: FaIcon(FontAwesomeIcons.wind),
+                              title: Text("Wind Speed"),
+                              trailing: Text(snapshot.data["results"]["wind_speedy"]),
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                );
+              }
+          }
+        },
+      )
+    );
+
+    /*Scaffold(
       backgroundColor: Colors.lightBlueAccent[700],
       body: FutureBuilder<Map>(
         future: getData(),
@@ -112,7 +218,7 @@ class _HomeState extends State<Home> {
           }
         },
       ),
-    );
+    );*/
   }
 }
 
